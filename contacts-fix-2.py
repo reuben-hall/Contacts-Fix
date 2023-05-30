277b9c39c635677d00a4cb3de3d989d9039aaf7b
reuben-hall <118365730+reuben-hall@users.noreply.github.com>
2023-05-30 22:14:03 +0100

    Create contacts-fix-2.py
    Created function for number checking

---
+++ contacts-fix-2.py
@@ -0,0 +1,44 @@
+import csv
+import re
+
+filename = '/Users/reuben/Development/Python/Contacts Fix/contacts-2.csv'
+
+# match_all_numbers = "[\(?\+\)?^0-9]"
+
+# Match  string 10 digits long not starting with 0
+is_us_number = "^[1-9]{10}"
+# Match string 11 digits long starting with 07
+is_uk_mobile_number = "07\d{9}"
+# Match string 11 digits long starting with 09
+is_ph_number = "09\d{9}"
+
+# Accept phone number as argument and run phone number detection on it
+def numbercheck(mobile_number):
+    if mobile_number:
+        # Replace ()- and spaces with no space
+        phone_number = mobile_number.replace('(','').replace(')','').replace(' ','').replace('-','')
+        
+        # Run US number regex pattern
+        if re.match(is_us_number, phone_number):
+            print(f'{phone_number} is probably US number.')
+            # Append US country code to left of string and print result
+            full_phone_number = "+1" + phone_number
+            print(f'Updated phone number is {full_phone_number}.')
+        elif re.match(is_uk_mobile_number, phone_number):
+            print(f'{phone_number} is probably UK mobile number.')
+            full_phone_number = "+44" + phone_number
+            print(f'Updated phone number is {full_phone_number}.')
+        elif re.match(is_ph_number, phone_number):
+            print(f'{phone_number} is probably PH number.')
+            full_phone_number = "+44" + phone_number
+            print(f'Updated phone number is {full_phone_number}.')
+        else:
+            return "Number not valid"
+    
+with open(filename, 'r') as csvfile:
+    contactsreader = csv.DictReader(csvfile)
+
+    line_count = 0
+
+    for row in contactsreader:
+        numbercheck(row["Mobile Phone"])
