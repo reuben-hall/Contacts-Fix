1055f64f6eb9cd088b5219c75ecd5c498fc7a505
reuben-hall <118365730+reuben-hall@users.noreply.github.com>
2023-05-30 17:41:35 +0100

    Trying to use match case to cycle through phone number variations

--- contacts-fix.py
+++ contacts-fix.py
@@ -3,6 +3,11 @@ import re
 
 filename = '/Users/reuben/Development/Python/Contacts Fix/contacts-2.csv'
 
+match_all_numbers = "[\(?\+\)?^0-9]"
+is_us_number = "^\(?\d{3}\)? ?\d{3} ?\d{4}\s"
+chars = "()- "
+
+
 with open(filename, 'r') as csvfile:
     contactsreader = csv.DictReader(csvfile)
 
@@ -11,16 +16,30 @@ with open(filename, 'r') as csvfile:
     for row in contactsreader:
 
         test_char = row["Mobile Phone"]
-        
-        if (test_char):
 
-            if test_char[0] == "0":
-                print(f'{test_char}' + ' starts with 0')
-            else:
-                print(f'{test_char}' + ' does not start with 0')
+        if test_char:
+            match test_char[0]:
+                case "+":
+                    # International number
+                    print(test_char.replace('-','').replace(' ',''))
+                case "(":
+                    # Local US Number
+                    print(test_char.replace('(','').replace(')','').replace(' ','').replace('-',''))
+                case _:
+                    print(f'{test_char} did not match any criteria.')
+            
+            
+
+
+        """ if (test_char):
+            if (test_char[0] == "+"):
+                print(f'Skipping {test_char}: starts with +.')
+
+            elif " " in test_char:
+                print(test_char.replace(" ",""))
         
         else:
-            print(f'-' * 75)
+            print(f'-' * 75) """
         
         line_count += 1
         
